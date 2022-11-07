0360b8df420cb3817f9065257d91248922f7d784
lorenzo-miuccio <lorenzo.miuccio@gmail.com>
2022-11-07 13:57:56 +0100

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent} from "./register/register.component";
+import { LoggedInPageComponent } from "./logged-in-page/logged-in-page.component";
+import {AppComponent} from "./app.component";
+
 
 const routes: Routes = [{
+  path: 'app', component: AppComponent},{
   path: 'login', component: LoginComponent},{
-  path: 'register', component: RegisterComponent}
+  path: 'register', component: RegisterComponent}, {
+  path: 'logged-in-page', component: LoggedInPageComponent}
 ];
 
 @NgModule({

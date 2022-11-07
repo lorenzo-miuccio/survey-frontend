390c683cd84e73d271a0dd4805ee227e93638119
lorenzo-miuccio <lorenzo.miuccio@gmail.com>
2022-11-07 14:22:58 +0100

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { LoggedInPageComponent } from './logged-in-page/logged-in-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { LoggedInPageComponent } from './logged-in-page/logged-in-page.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

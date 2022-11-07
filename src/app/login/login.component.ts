db16d6bc63418729eecc7a67f0a29202632eeeab
lorenzo-miuccio <lorenzo.miuccio@gmail.com>
2022-11-07 20:53:38 +0100

    ok

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog, MatDialogConfig, MatDialogRef} from "@angular/material/dialog";
+import {DialogTemplateComponent} from "../dialog-template/dialog-template.component";
+import {AppComponent, Button} from "../app.component";
+import {RegisterComponent} from "../register/register.component";
+import {LoggedInPageComponent} from "../logged-in-page/logged-in-page.component";
 
 @Component({
   selector: 'app-login',
@@ -7,7 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog, public dialogRef: MatDialogRef<any>) { }
+
+  openRegister() {
+
+    this.dialogRef.close();
+    const config = new MatDialogConfig();
+
+    let b2 = new Button("CANCEL", false, false);
+    let b1 = new Button("ENTER", true, true);
+
+    let buttons: Button[] = [b2, b1];
+
+    config.disableClose = true;
+    config.id           = "register-component";
+    config.height       = "500px";
+    config.width        = "400px";
+    config.data = {title: 'REGISTER',
+      component: 'register',
+      buttons: buttons
+    }
+
+    let dialogRef = this.dialog.open(DialogTemplateComponent, config);
+
+  }
 
   ngOnInit(): void {
   }
